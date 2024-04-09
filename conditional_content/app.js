ab75b74606a316f87d45297e07d5f90bc90bc6b6
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-09 07:18:13 -0400

    Removing List Items

--- conditional_content/app.js
+++ conditional_content/app.js
@@ -9,6 +9,9 @@ const app = Vue.createApp({
       }
       this.goals.push(this.enteredGoal);
       this.enteredGoal = "";
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
     }
   },
 });
