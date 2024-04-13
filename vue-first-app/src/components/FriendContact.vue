8543d70cbf1c1fbe18d70a4bbc299d309c28a2ed
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-13 19:20:32 -0400

    Prop Behavior & Changing Props

--- vue-first-app/src/components/FriendContact.vue
+++ vue-first-app/src/components/FriendContact.vue
@@ -1,26 +1,21 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
+    <h2>{{ name }}</h2>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide" : "Show" }} Details
     </button>
     <ul v-if="detailsAreVisible">
-      <li><strong>Phone:</strong> {{ friend.phone }}</li>
-      <li><strong>Email:</strong> {{ friend.email }}</li>
+      <li><strong>Phone:</strong> {{ phone }}</li>
+      <li><strong>Email:</strong> {{ email }}</li>
     </ul>
   </li>
 </template>
 
 <script>
 export default {
+  props: ["name", "phone", "email"],
   data() {
     return {
-      friend: {
-        id: "manuel",
-        name: "Manuel Lorenz",
-        phone: "01234 5678 991",
-        email: "manuel@localhost.com",
-      },
       detailsAreVisible: false,
     };
   },
