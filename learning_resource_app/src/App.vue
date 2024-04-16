1a64c8251cf992a89a284240ddc46d557e5b8e7e
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-16 07:21:31 -0400

    project 1 set up

---
+++ learning_resource_app/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource.id">
+      {{ resource.title }}
+      {{ resource.description }}
+      {{ resource.link }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import LearningResource from "./components/LearningResource.vue";
+export default {
+    components: {
+        LearningResource,
+    },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation.",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to Google...",
+          link: "https://www.google.com",
+        }
+      ],
+    };
+  },
+};
+</script>
+
+
+<style></style>
---
+++ learning_resource_app/src/components/LearningResource.vue
@@ -0,0 +1,7 @@
+<template>learning resource item</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
