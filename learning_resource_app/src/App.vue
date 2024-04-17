19015875d7f4a6334707e78fce24342791fa3317
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-17 08:16:53 -0400

    Header & BaseCard Components

--- learning_resource_app/src/App.vue
+++ learning_resource_app/src/App.vue
@@ -1,20 +1,10 @@
-<template>
-  <ul>
-    <learning-resource
-      v-for="resource in storedResources"
-      :key="resource.id"
-      :title="resource.title"
-      :description="resource.description"
-      :link="resource.link"
-    ></learning-resource>
-  </ul>
-</template>
-
 <script>
-import LearningResource from "./components/learning-resources/LearningResource.vue";
+import TheHeader from "./components/layouts/TheHeader.vue";
+import StoredResources from "./components/learning-resources/StoredResources.vue";
 export default {
   components: {
-    LearningResource,
+    TheHeader,
+    StoredResources,
   },
   data() {
     return {
@@ -37,4 +27,23 @@ export default {
 };
 </script>
 
-<style></style>
+<template>
+  <the-header title="Learning Resources"></the-header>
+  <stored-resources :resources="storedResources"> </stored-resources>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
