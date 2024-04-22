a31cca19a4038f13b4fd6c80ff9109aa10fb847c
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-22 07:56:32 -0400

    v-model and Dropdowns

--- working_with_forms/src/components/TheForm.vue
+++ working_with_forms/src/components/TheForm.vue
@@ -2,15 +2,26 @@
   <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model.trim="userName" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="userName"
+      />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" v-model.number="userAge" ref="ageInput" />
+      <input
+        id="age"
+        name="age"
+        type="number"
+        v-model.number="userAge"
+        ref="ageInput"
+      />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -58,15 +69,18 @@ export default {
     return {
       userName: "",
       userAge: null,
+      referrer: "wom",
     };
   },
   methods: {
     submitForm() {
       console.log(this.userName);
       this.userName = "";
-      console.log(this.userAge + 342);; // this yields a number
+      console.log(this.userAge + 342); // this yields a number
       console.log(this.$refs.ageInput.value + 342); // this yields a string
       this.userAge = null;
+      console.log("Referrer: " + this.referrer);
+      this.referrer = "wom";
     },
   },
   computed: {},
