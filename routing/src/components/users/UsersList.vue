fe923d7637b3a6a9c59e4ca0b1396e79e002565b
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-24 06:49:17 -0400

    Programmatic Navigation

--- routing/src/components/users/UsersList.vue
+++ routing/src/components/users/UsersList.vue
@@ -1,4 +1,5 @@
 <template>
+  <button @click="confirmInput">Confirm</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -12,6 +13,15 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  methods: {
+    confirmInput() {
+      // do task
+
+      // at the end of task,
+      // navigate to another page
+      this.$router.push('/teams');
+    },
+  },
 };
 </script>
 
