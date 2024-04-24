a6a80e5208cb9473f1ca2f5cfe337097e3a91549
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-24 07:44:45 -0400

    Redirecting & "Catch All" Routes

--- routing/src/main.js
+++ routing/src/main.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import TeamsList from "./components/teams/TeamsList.vue";
 import UsersList from "./components/users/UsersList.vue";
 import TeamMembers from "./components/teams/TeamMembers.vue";
+import TheNotFound from "./components/not-found/TheNotFound.vue";
 import App from "./App.vue";
 
 const router = createRouter({
@@ -12,6 +13,7 @@ const router = createRouter({
     { path: "/teams", component: TeamsList },
     { path: "/users", component: UsersList },
     { path: "/teams/:teamId", component: TeamMembers, props: true},
+    { path: "/:notFound(.*)", component: TheNotFound }
   ],
   linkActiveClass: "custom-class",
 });
