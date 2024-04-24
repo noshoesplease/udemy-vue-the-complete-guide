f9985a9f28950f21fcd62b5c31a0f6c4e804d37f
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-24 06:35:32 -0400

    Registering & Rendering Routes

--- routing/src/main.js
+++ routing/src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
-
+import TeamsList from "./components/teams/TeamsList.vue";
 import App from "./App.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-   
+    { path: "/", redirect: "/teams" },
+    { path: "/teams", component:  TeamsList},
+    { path: "/users", component: () => import("./components/users/UsersList.vue") },
   ],
 });
 
