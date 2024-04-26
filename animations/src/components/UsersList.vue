a85e0b51fe87af806c3955d595d6e6cc5fe56dc6
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-26 09:23:06 -0400

    Animate List Item Movement

--- animations/src/components/UsersList.vue
+++ animations/src/components/UsersList.vue
@@ -65,9 +65,14 @@ input {
 }
 .user-list-leave-active {
   transition: all 1s ease-in;
+  position: absolute;
 }
 .user-list-leave-to {
   opacity: 0;
   transform: translateX(30px);
+}
+
+.user-list-move {
+  transition: transform 1s ease-out;
 }
 </style>
