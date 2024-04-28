99c4f8e698958094a558958c62d425f6eca84b44
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-27 20:30:39 -0400

    Introducing Getters - A Better Way Of Getting Data

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <base-container title="Vuex">
     <the-counter></the-counter>
+    <favorite-value></favorite-value>
     <change-counter></change-counter>
     <button @click="addTen">Add 10</button>
   </base-container>
@@ -10,11 +11,14 @@
 import BaseContainer from "./components/BaseContainer.vue";
 import TheCounter from "./components/TheCounter.vue";
 import ChangeCounter from "./components/ChangeCounter.vue";
+import FavoriteValue from "./components/FavoriteValue.vue";
+
 export default {
   components: {
     BaseContainer,
     TheCounter,
     ChangeCounter,
+    FavoriteValue,
   },
 
   methods: {
