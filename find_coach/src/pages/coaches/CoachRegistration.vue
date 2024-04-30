a509e5f703726d116a40af94ffedfc84e8c79f68
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-30 09:32:33 -0400

    Registering as a Coach: The Form

--- find_coach/src/pages/coaches/CoachRegistration.vue
+++ find_coach/src/pages/coaches/CoachRegistration.vue
@@ -1,9 +1,19 @@
 <template>
-    COACH REGISTRATION PAGE
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {};
+import CoachForm from "@/components/coaches/CoachForm.vue";
+export default {
+  components: {
+    CoachForm,
+  },
+};
 </script>
 
 <style scoped></style>
