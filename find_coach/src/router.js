4da14e31ca15f79f369645cf4e3d506770ae8020
Devin Custodio <devin.roy.custodio@gmail.com>
2024-04-29 21:36:37 -0400

    Registering Routes

---
+++ find_coach/src/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    { path: "/", redirect: "/coaches" },
+    { path: "/coaches", component: null },
+    {
+      path: "/coaches/:id",
+      component: null,
+      children: [{ path: "contact", component: null }],
+    },
+    { path: "/register", component: null },
+    { path: "/requests", component: null },
+    { path: "/:notFound(.*)", component: null },
+  ],
+});
+export default router;
