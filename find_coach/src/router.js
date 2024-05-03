cb761a1db7b2f78a300bcb6fa26f9c2612924fcc
Devin Custodio <devin.roy.custodio@gmail.com>
2024-05-03 08:59:59 -0400

    fixed router bug

--- find_coach/src/router.js
+++ find_coach/src/router.js
@@ -1,13 +1,14 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
+
 import CoachDetail from "./pages/coaches/CoachDetail.vue";
 import CoachesList from "./pages/coaches/CoachesList.vue";
 import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
 import ContactCoach from "./pages/requests/ContactCoach.vue";
-import RequestReceived from "./pages/requests/RequestsReceived.vue";
+import RequestsReceived from "./pages/requests/RequestsReceived.vue";
 import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/coaches" },
     { path: "/coaches", component: CoachesList },
@@ -15,11 +16,14 @@ const router = createRouter({
       path: "/coaches/:id",
       component: CoachDetail,
       props: true,
-      children: [{ path: "contact", component: ContactCoach }],
+      children: [
+        { path: "contact", component: ContactCoach }, // /coaches/c1/contact
+      ],
     },
     { path: "/register", component: CoachRegistration },
-    { path: "/requests", component: RequestReceived },
+    { path: "/requests", component: RequestsReceived },
     { path: "/:notFound(.*)", component: NotFound },
   ],
 });
+
 export default router;
