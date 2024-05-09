03fe535601fdd0197f0605d8ad7339f5c55a42af
Devin Custodio <devin.roy.custodio@gmail.com>
2024-05-09 10:01:30 -0400

    Adding Route Transitions

--- find_coach/src/pages/requests/RequestsReceived.vue
+++ find_coach/src/pages/requests/RequestsReceived.vue
@@ -1,26 +1,28 @@
 <template>
-   <base-dialog title="An error occurred" :show="!!error" @close="handleError">
-    <p>{{ error }}</p>
-  </base-dialog>
-  <section>
-    <base-card>
-      <header>
-        <h2>Requests Received</h2>
-      </header>
-      <div v-if="isLoading">
-        <base-spinner></base-spinner>
-      </div>
-      <ul v-else-if="hasRequests && !isLoading">
-        <request-item
-          v-for="request in receivedRequests"
-          :key="request.id"
-          :email="request.userEmail"
-          :message="request.message"
-        ></request-item>
-      </ul>
-      <h3 v-else>You haven't received any requests yet</h3>
-    </base-card>
-  </section>
+  <div>
+    <base-dialog title="An error occurred" :show="!!error" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <section>
+      <base-card>
+        <header>
+          <h2>Requests Received</h2>
+        </header>
+        <div v-if="isLoading">
+          <base-spinner></base-spinner>
+        </div>
+        <ul v-else-if="hasRequests && !isLoading">
+          <request-item
+            v-for="request in receivedRequests"
+            :key="request.id"
+            :email="request.userEmail"
+            :message="request.message"
+          ></request-item>
+        </ul>
+        <h3 v-else>You haven't received any requests yet</h3>
+      </base-card>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -32,7 +34,7 @@ export default {
   data() {
     return {
       isLoading: false,
-      error: null
+      error: null,
     };
   },
   computed: {
@@ -52,7 +54,7 @@ export default {
     },
     async fetchRequests() {
       this.isLoading = true;
-      try{
+      try {
         await this.$store.dispatch("requests/fetchRequests");
       } catch (error) {
         this.error = error.message || "Failed to fetch requests";
