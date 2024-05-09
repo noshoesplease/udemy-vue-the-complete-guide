4324c3890b04701931b3f2b35c01364bceef5241
Devin Custodio <devin.roy.custodio@gmail.com>
2024-05-09 08:21:57 -0400

    Sending a PUT Http Request to Store Coach Data

--- find_coach/src/store/modules/coaches/actions.js
+++ find_coach/src/store/modules/coaches/actions.js
@@ -1,13 +1,30 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
-    context.commit("registerCoach", coachData);
+
+    const fetchURL = import.meta.env.VITE_FIREBASE_URL.replace('coaches.json', `coaches/${userId}.json`);
+    const response = await fetch(fetchURL, {
+      method: "PUT",
+      body: JSON.stringify(coachData),
+    });
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error ...
+    }
+
+
+    context.commit("registerCoach", {
+      ...coachData,
+      id: userId,
+    });
   },
 };
