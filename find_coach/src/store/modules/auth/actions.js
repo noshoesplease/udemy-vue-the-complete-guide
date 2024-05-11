582d322613f32caae25899540a6f56a8e00a64c8
Devin Custodio <devin.roy.custodio@gmail.com>
2024-05-11 10:02:09 -0400

    Adding "Auto Login"

--- find_coach/src/store/modules/auth/actions.js
+++ find_coach/src/store/modules/auth/actions.js
@@ -1,8 +1,15 @@
 export default {
-  async login(context, payload) {
-    const fetchURL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${
-      import.meta.env.VITE_FIREBASE_AUTH_API_KEY
-    }`;
+  async auth(context, payload) {
+    const { mode } = payload;
+    const signUp = "signUp";
+    const signInWithPassword = "signInWithPassword";
+    const modeMap = {
+      signUp,
+      signInWithPassword,
+    };
+    const fetchURL = `https://identitytoolkit.googleapis.com/v1/accounts:${
+      modeMap[mode]
+    }?key=${import.meta.env.VITE_FIREBASE_AUTH_API_KEY}`;
 
     const response = await fetch(fetchURL, {
       method: "POST",
@@ -15,18 +22,38 @@ export default {
     const responseData = await response.json();
 
     if (!response.ok) {
-      console.log(responseData);
       const error = new Error(
-        responseData.message || "Failed to authenticate. Check your login data."
+        responseData.message || "Failed to authenticate. Try again later."
       );
       throw error;
     }
 
-    console.log(responseData);
+    localStorage.setItem("token", responseData.idToken);
+    localStorage.setItem("userId", responseData.localId);
+
     context.commit("setUser", {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn,
+    });
+  },
+
+  tryLogin(context) {
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
+    if (token && userId) {
+      context.commit("setUser", {
+        token,
+        userId,
+        tokenExpiration: null,
+      });
+    }
+  },
+
+  async login(context, payload) {
+    return context.dispatch("auth", {
+      ...payload,
+      mode: "signInWithPassword",
     });
   },
 
@@ -39,33 +66,9 @@ export default {
   },
 
   async signup(context, payload) {
-    const fetchURL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${
-      import.meta.env.VITE_FIREBASE_AUTH_API_KEY
-    }`;
-
-    const response = await fetch(fetchURL, {
-      method: "POST",
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true,
-      }),
-    });
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(
-        responseData.message || "Failed to authenticate. Check your login data."
-      );
-      throw error;
-    }
-
-    console.log(responseData);
-    context.commit("setUser", {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
+    return context.dispatch("auth", {
+      ...payload,
+      mode: "signUp",
     });
   },
 };
